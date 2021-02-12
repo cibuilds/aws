296bbeeba7ec21af3a45f224850bf8ed14f08197
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-02-12 12:54:20 -0500

    Add AWS CLI v2.1.25. (#754)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.19/Dockerfile -t cibuilds/aws:1.19.5  -t cibuilds/aws:1.19 .
+docker build --file 2.1/Dockerfile -t cibuilds/aws:latest -t cibuilds/aws:2.1.25  -t cibuilds/aws:2.1 .
