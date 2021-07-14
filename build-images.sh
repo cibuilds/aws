bfb305847ba213234a36231bee980ce52eb56778
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-07-14 00:14:10 -0400

    Add AWS CLI v1.19.111. (#895)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.19/Dockerfile -t cibuilds/aws:1.19.110  -t cibuilds/aws:1.19 .
+docker build --file 1.19/Dockerfile -t cibuilds/aws:1.19.111  -t cibuilds/aws:1.19 .
