5ab251af47312e0c128c9cd7aadd91b10a8d6991
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-18 14:01:51 -0400

    Add AWS CLI v2.8.3. (#1148)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 2.8/Dockerfile -t cibuilds/aws:latest -t cibuilds/aws:2.8.2  -t cibuilds/aws:2.8 .
+docker build --file 2.8/Dockerfile -t cibuilds/aws:latest -t cibuilds/aws:2.8.3  -t cibuilds/aws:2.8 .
